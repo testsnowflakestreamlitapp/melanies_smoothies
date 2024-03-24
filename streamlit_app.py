2e1abdc0e97ed01d98568d33f7e5b3aed94e260d
testsnowflakestreamlitapp <164714110+testsnowflakestreamlitapp@users.noreply.github.com>
2024-03-24 00:21:51 -0500

    Update streamlit_app.py
    Moved the fruity vice data display to the top

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title("Customize Your Smoothie! :cup_with_straw:")
@@ -29,6 +30,8 @@ if ingredients_list:
 
     for fruit_choosen in ingredients_list:
         ingredients_string += fruit_choosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -45,8 +48,4 @@ if ingredients_list:
         st.success('Your Smoothie is ordered,'+ name_on_order + '!', icon="✅")
 
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
 
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
