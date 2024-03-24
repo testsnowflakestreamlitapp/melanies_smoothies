c30add344967300852a1ace5b907f732f527bcdb
testsnowflakestreamlitapp <164714110+testsnowflakestreamlitapp@users.noreply.github.com>
2024-03-24 00:25:34 -0500

    Update streamlit_app.py
    Update the code to show the nutrition information only for the selected fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,8 @@ if ingredients_list:
 
     for fruit_choosen in ingredients_list:
         ingredients_string += fruit_choosen + ' '
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        st.subheader(fruit_choosen + 'Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
